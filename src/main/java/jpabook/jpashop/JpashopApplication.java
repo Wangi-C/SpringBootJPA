7893090aceb8ff81be39f8fbc103b8d2bc90c971
Wangi-C <kiiiiwcho@gmail.com>
2022-12-28 23:32:32 +0900

    라이브러리 살펴보기 spring-boot-starter-? : 필요한 라이브러리를 자동으로 의존주입해준다. 핵심 라이브러리  - 스프링mvc  - 스프링orm  - jpa,하이버네이트  - 스프링 데이터jpa

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		Hello hello = new Hello();
+		hello.setStr("hello");
+		System.out.println(hello.getStr());
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
